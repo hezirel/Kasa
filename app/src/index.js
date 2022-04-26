5757ce6d10b11020f197b4e08941a646f4ae3c42
hezirel <80560775+hezirel@users.noreply.github.com>
2022-04-26 19:47:10 +0200

    added Routing prototype => need to establish way to transmit product id over URL params

--- app/src/index.js
+++ app/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
 import Header from "./components/Header";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<React.StrictMode>
 		<Header />
-		<App />
+		<Router>
+			<Routes>
+				<Route path="/" element={<App />}/>
+				<Route path="/housing" element={<Header />}/>
+				<Route path="/about" element={<Header />}/>
+			</Routes>
+		</Router>
 	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
