dc5cc03283c6bcfbd9789c8277628170a1b804a7
hezirel <80560775+hezirel@users.noreply.github.com>
2022-04-26 20:07:43 +0200

    Converted Header HTML to JSX

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,8 +4,20 @@ import React from "react";
 
 const Header = () => {
 	return (
-		<header>
-			<img src={logo} className="logo" alt="logo" />
+		<header className="header-main">
+			<div className="header-container">
+				<img src={logo} alt="logo"/>
+				<nav className="header-nav">
+					<ul className="header-nav-list">
+						<li className="header-nav-item">
+							<a href="/" className="header-nav-link">Acceuil</a>
+						</li>
+						<li className="header-nav-item">
+							<a href="housing" className="header-nav-link">A Propos</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
