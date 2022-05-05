b99cd0ca3ea9dc3f052d8f70c9cd8b4df1b65016
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-06 00:13:26 +0200

    Switch router to Hash Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
