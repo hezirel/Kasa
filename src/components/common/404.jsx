45df6fddc9c8f9ee37d0df638ed643c1b0f27914
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-10 11:19:46 +0200

    404 css changes

--- src/components/common/404.jsx
+++ src/components/common/404.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./404.css";
 
 
 const NotFound = () => {
 	return (
-		<div className="notFound"><p>404</p></div>
+		<div className="App divError">
+			<h3>404</h3>
+			<p>Oups ! La page que vous demandez n&apos;existe pas</p>
+			<Link to="/">
+			Retourner sur la page d’accueil
+			</Link>
+		</div>
 	);
 };
 
